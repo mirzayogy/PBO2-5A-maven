efa90034195d12794a0eb8e4f07612c6ea8b7985
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2021-01-04 13:00:45 +0800

    database db_5a

---
+++ src/main/java/database/FileSQL.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author mirza
+ */
+public class FileSQL {
+    
+    /*
+    
+    Buat database db_5a
+
+
+CREATE TABLE `mahasiswa` (
+  `npm` varchar(10) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `ipk` double DEFAULT 0,
+  `jumlah_matakuliah` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `mahasiswa` (`npm`, `nama`, `ipk`, `jumlah_matakuliah`) VALUES
+('19630001', 'Wahiddd', 3.12, 12),
+('19630002', 'Dwi', 3.2, 12),
+('19630003', 'Tri', 3.33, 13),
+('19630007', 'Pitung', 3.17, 17);
+
+
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`npm`);
+COMMIT;
+
+*/
+    
+}
